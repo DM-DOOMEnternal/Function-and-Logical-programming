f578acd6f44183bc90d0619f299f4fa8e9aea7c5
StrongerProgrammer7 <96116317+DM-DOOMEnternal@users.noreply.github.com>
2022-03-17 09:55:32 +0300

    Task 1-41
    Дан целочисленный массив. Найти среднее арифметическое
    модулей его элементов.

---
+++ LB7/Task1-41/Task1-41/Program.fs
@@ -0,0 +1,36 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+(*Дан целочисленный массив. Найти среднее арифметическое
+модулей его элементов.*)
+
+let rec readList n =
+    if n=0 then []
+    else
+    let Head = System.Convert.ToInt32(System.Console.ReadLine())
+    let Tail = readList (n-1)
+    Head::Tail
+
+(*let readSizeList =
+    printfn "Задайте размер листа, затем вводите последовательно числа"
+    let n = System.Convert.ToInt32(System.Console.ReadLine())
+    readList n*)
+
+let rec outPutList list =
+    match list with
+    |head::[] -> printfn "%A" head
+    |head::tail-> printf "%A," head; outPutList tail
+    |[]-> printfn ""
+
+
+[<EntryPoint>]
+let main argv =
+    let list = [5.0;3.0;-2.0;4.0;-1.0;-2.0;]
+    outPutList list
+    let result = List.fold(fun acc elm -> (Math.Abs(int elm))+acc) 0 list
+    printfn " Summa : %d" result
+    let result2 = (double result)/(double (List.length(list)))
+    printfn " Result %f " (double result2)
+    0 // return an integer exit code
+    (*Дан целочисленный массив. Найти среднее арифметическое
+    модулей его элементов.*)
