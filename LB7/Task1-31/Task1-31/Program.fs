d836d151c994c45524b0db1e8898f881bbb06214
StrongerProgrammer7 <96116317+DM-DOOMEnternal@users.noreply.github.com>
2022-03-17 09:44:05 +0300

    Task1-31
    Дан целочисленный массив. Найти количество чётных элементов.

---
+++ LB7/Task1-31/Task1-31/Program.fs
@@ -0,0 +1,32 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+(*Дан целочисленный массив. Найти количество чётных элементов.*)
+let rec readList n =
+    if n=0 then []
+    else
+    let Head = System.Convert.ToInt32(System.Console.ReadLine())
+    let Tail = readList (n-1)
+    Head::Tail
+
+(*let readSizeList =
+    printfn "Задайте размер листа, затем вводите последовательно числа"
+    let n = System.Convert.ToInt32(System.Console.ReadLine())
+    readList n*)
+
+let rec outPutList list =
+    match list with
+    |head::[] -> printfn "%A" head
+    |head::tail-> printf "%A," head; outPutList tail
+    |[]-> printfn ""
+
+
+[<EntryPoint>]
+let main argv =
+    let list = [2;4;5;3;2;1;6;8;9]
+    outPutList list
+    let list2 = List.filter(fun x -> x%2=0) list
+    outPutList list2
+    printfn " Count odd elems : %d " (List.length(list2))
+    0 // return an integer exit code
+    (*Дан целочисленный массив. Найти количество чётных элементов.*)
