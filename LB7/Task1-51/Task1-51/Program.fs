9e3418b18688e7036f4bcbc165010d066e8f15cf
StrongerProgrammer7 <96116317+DM-DOOMEnternal@users.noreply.github.com>
2022-03-22 12:25:26 +0300

    Modern

--- LB7/Task1-51/Task1-51/Program.fs
+++ LB7/Task1-51/Task1-51/Program.fs
@@ -23,7 +23,7 @@ let rec outPutList list =
     |head::tail-> printf "%A," head; outPutList tail
     |[]-> printfn ""
 
-
+(*
 let noRepeat list (x,y) =
     let rec notrep list (x,y) c =
         match list with
@@ -33,7 +33,8 @@ let noRepeat list (x,y) =
         |h::t when c>1 -> false
         |h::t -> notrep t (x,y) c
     notrep list (x,y) 0
-
+*)
+(*
 let repeat list (x,y) =
     let rec rep list (x,y) (i,c) =
         match list with
@@ -41,17 +42,39 @@ let repeat list (x,y) =
         |h::t when (snd(h))=y -> rep t (x,y) (i,(c+1))
         |h::t -> rep t (x,y) (i,c)
     rep list (x,y) (x,0)
+*)
+let getFirst (c,_,_) = c
+let getSnd (_,c) = c
+let getThird (_,_,c) = c
+
+let quael elm1 list2 = List.exists(fun x -> (elm1=(fst(x))) && (snd(x))=1 ) list2 
+
+//let repeat1 (x,y) res  = List.find(fun z -> snd(fst((z)))=y && fst(z)=x) res
+
+//let search y L2 = List.find (fun z -> y=(fst(snd(z)))) L2
+
+//let repeat (x,y) L  = List.find(fun z -> fst(z)=x && (search y L)=y ) L
+
+let Rp x L = List.exists(fun z -> snd(z)=fst(x) && snd(x)=1) L
 
 [<EntryPoint>]
 let main argv =
     let list = [2;3;4;5;4;3;1]
     outPutList list
-    let list2 = List.filter(fun elm -> noRepeat list elm) (List.indexed(list))
+    //let list2 = List.filter(fun elm -> noRepeat list elm) (List.indexed(list))
+    let res = list |> List.countBy id
+    printfn " Частота встречаемости элементов  в исходном списке: "
+    outPutList res //(List.indexed(List.rev(List.sortBy (fun (a,b) -> b=1) res)))
+    let L1 = List.indexed(List.filter(fun lx  -> quael lx res ) list )
     printfn "элементы L1 это неповторяющиеся элементы исходного списка "
-    outPutList list2
-    let list3 = List.map(fun x -> repeat (List.indexed(list)) x) list2  
+    outPutList L1
+   // let l2 = (List.indexed(List.rev(List.sortBy (fun (a,b) -> b=1) res)))
+   // outPutList l2
+    //let list3 = List.map(fun x -> repeat (List.indexed(list)) x) list2  
+   // let list3 = List.map(fun x -> (fst(x), repeat x l2) ) L1
+    let L2 = List.indexed(List.filter(fun x -> snd(x)=1 && Rp x L1 ) res)
     printfn "элемент списка L2 с номером i показывает, сколько раз элемент списка L1 с таким номером повторяется в исходном "
-    outPutList list3
+    outPutList L2
     0 // return an integer exit code
     (*Для введенного списка построить два списка L1 и L2, где элементы L1
     это неповторяющиеся элементы исходного списка, а элемент списка L2 с
