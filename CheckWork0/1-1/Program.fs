3c780276a7a570fe1f1e32e2067a7cbe08948918
StrongerProgrammer7 <96116317+DM-DOOMEnternal@users.noreply.github.com>
2022-03-25 15:43:54 +0300

    CheckWork
    Реализовать функцию, вычисляющую произведение четных элементов списка с
    помощью списков Черча и рекурсии вверх или вниз
    Реализовать функцию, вычисляющую количество делителей числа n с помощью
    рекурсии вверх или вних

---
+++ CheckWork0/1-1/Program.fs
@@ -0,0 +1,61 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+(*Реализовать функцию, вычисляющую количество делителей числа n с помощью
+рекурсии вверх или вних*)
+
+let countDelDown num =
+    let rec count constNum num  acc =
+        match num with
+        |num when num=0 -> acc
+        |num when (constNum%num=0) -> count constNum (num-1) (acc+1)
+        |num -> count constNum (num-1) acc
+    count num num 0
+
+let rec countDelUp constNum num = 
+    match num with
+    |num when num=0 -> 0
+    |num when (constNum%num=0) -> 1+countDelUp constNum (num-1)
+    |num -> countDelUp constNum (num-1)
+
+(*Реализовать функцию, вычисляющую произведение четных элементов списка с
+помощью списков Черча и рекурсии вверх или вниз*)
+
+(*
+let rec writeList n =
+    if(n=0) then []
+    else
+        let h = System.Convert.ToInt32(System.Console.ReadLine())
+        let t = writeList (n-1)
+        h::t
+
+let readSizeList =
+    printfn " Size List : "
+    let size = System.Convert.ToInt32(System.Console.ReadLine())
+    writeList size
+*)
+
+let rec outputList list =
+    match list with 
+    |[]-> printf "]"
+    |h::t -> printfn "%d," h;outputList t
+
+let odd x = x%2=0
+
+let multOddElm list fOdd =
+    let rec multElm list fOdd acc =
+        match list with
+        |[] -> acc
+        |h::t when (fOdd h) -> multElm t fOdd (h*acc)
+        |h::t -> multElm t fOdd acc
+    multElm list fOdd 1
+
+[<EntryPoint>]
+let main argv =
+    printfn "Result Down : %d" (countDelDown 50)
+    printfn "Result Up : %d" (countDelUp 50 50)
+    let list = [5;4;2;1;3;5]
+    let multOddElmList = fun f x funcOdd -> printfn " Mult Odd elements list : %d" (f x funcOdd)
+    let result = multOddElmList multOddElm list odd
+    0 // return an integer exit code
---
+++ Fibonachi_Factorial/Fib_Fact.fs
@@ -0,0 +1,54 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let fib n =
+    let rec loop acc1 acc2 n =
+        match n with
+        | 0 -> acc1
+        | 1 -> acc2
+        | _ ->
+            loop acc2 (acc1 + acc2) (n - 1)
+    loop 0 1 n
+    
+let rec fib2 n =
+    match n with
+    |1 |2 -> 1
+    |_ -> fib2 (n-1) + fib2(n-2)
+
+let rec pr n =
+    match n with
+    |1 -> printfn "%d" (fib2 n)
+    |_-> pr (n-1); printfn "%d" (fib2 n)
+
+
+let fact k =
+    let rec f acc n =
+        match n with 
+        |n when n=1 -> acc
+        |n when n<>0 -> f (acc*n) (n-1)
+    f 1 k
+
+let odd x = x%2<>0
+
+let sumOdd list func =
+    let rec summ list func acc =
+        match list with
+        |[]-> acc
+        |h::t when func h -> summ t func (acc+h)
+        |h::t -> summ t func acc
+    summ list func 0
+
+[<EntryPoint>]
+let main argv =
+    //printfn "%d" (fib 16)
+    //pr 8
+    //printfn "%d" (fact 5)
+    let list = [1;2;4;2;5;3;1]
+    //let res = sumOdd list odd
+   // printfn " %d " res
+    let res = fun f x fodd -> printf "%d" (f x fodd)
+    res sumOdd list odd
+    //let count = fun f (x:int) (y:int) countfun -> printfn "Количесво взаимно простых с %d : %d " x (f x y countfun)
+    //count linkcount 58 0 searchSimpleNumber
+    0 // return an integer exit code
