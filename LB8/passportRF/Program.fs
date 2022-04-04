b10705c47c7da41a9c2d1f02e640d69339cc45e7
StrongerProgrammer7 <96116317+DM-DOOMEnternal@users.noreply.github.com>
2022-04-04 17:24:44 +0300

    Task 8
    Добавлена валидация полей регулярками

--- LB8/passportRF/Program.fs
+++ LB8/passportRF/Program.fs
@@ -2,81 +2,74 @@
 
 open System
 open System.Text.RegularExpressions
+open System.Text
+open System.Collections.Generic
 
 (*5 Создать класс, содержащий информацию о документе.*)
 
 type Document_PassportRF() =
 
-    let mutable _serial_Passport : int list =[]
-    let mutable _number_Passport : int list= []
+    let mutable _serial_Passport : string = "" 
+    let mutable _number_Passport : string = ""
     let mutable _surnmae_Passport : string = ""
     let mutable _name_Passport : string = ""
     let mutable _patronymic_Passport : string = ""
     let mutable _birthPlace : string =""
     let mutable _birthday : string ="01.01.1900"
 
-    let checkDigit x = 
-        match x with
-        |x when x%10=x -> true
-        |x when x%10<>x -> false
-        |x -> false
-    
-    let rec inputDigit a checkDig=
-         match a with
-         |a when checkDig a = true -> a
-         |a when checkDig a = false -> printf " Неверный ввод, введите заново(одна цифра)!"
-                                       inputDigit (System.Convert.ToInt32(System.Console.ReadLine())) checkDig
-         |a -> a
-    let rec serial_numberList n =
-           if n=0 then []
-           else
-                let mutable a = System.Convert.ToInt32(System.Console.ReadLine())       
-                let Head = inputDigit a checkDigit
-                let Tail = serial_numberList (n-1)
-                Head::Tail
-    
-    (*let rec check (x : string) =
-        match x with
-        |x when x.Length<6 || x.Length >6 -> printfn " Слишком коротко или длинно!(Длина = 6!)"; check (System.Console.ReadLine())
-        |x when x.Length = 6 && *)
-
-
-    (*let rec serialString n =
-        if(n=0) then ""
+    let rec checkInputSerial_Number (x : string) (y:string) =
+        let reg = new Regex(@"^([\d]+)$")
+        if(y="serial") then
+            match x with
+            |x when x.Length<6 || x.Length >6 -> printfn " Слишком коротко или длинно!(Длина = 6!) Введите заново! "; checkInputSerial_Number (System.Console.ReadLine()) y
+            |x when x.Length = 6 &&  reg.IsMatch(x) -> x
+            |x -> printfn " Неверно, 6 цифр! Введите заново!"; checkInputSerial_Number (System.Console.ReadLine()) y
         else
-            let mutable a = System.Console.ReadLine()*)
+            match x with
+            |x when x.Length<4 || x.Length >4 -> printfn " Слишком коротко или длинно!(Длина = 4!) Введите заново! "; checkInputSerial_Number (System.Console.ReadLine()) y
+            |x when x.Length = 4 &&  reg.IsMatch(x) -> x
+            |x -> printfn " Неверно, 4 цифр! Введите заново!"; checkInputSerial_Number (System.Console.ReadLine()) y
 
+    let serial_NumberPas (typeInput : string) = checkInputSerial_Number (System.Console.ReadLine()) typeInput
 
-    (*let rec writeBirthday data : string =
-        let dataformat = "dd-MM-yyyy"
-        let dat = data// DateTime.Parse(data)  
-        match dat with
-        |data when Int32.Parse(dat.Split(' ')[1])=2 -> 
-            printf " Не верно введена дата (дд,мм,гггг)"
-            writeBirthday (System.Console.ReadLine())
-        |data -> (string (data)) *)
+    let rec checkDateBirth (x :string) =
+        let reg = new Regex(@"^(0[1-9]|[1-2][0-9]|3[0-1])[- /.](0[1-9]|1[0-1-2])[- /.](19|20)\d\d$")
+        match x with
+        |x when x.Length<>0 && reg.IsMatch(x) -> x
+        |x -> printfn "Введено неверно, придерживайтесь формата: dd.mm.yyyy"; checkDateBirth (System.Console.ReadLine())
+
+    let rec check_Name_Sur_Patr (x:string) =
+        let reg = new Regex(@"^[a-zA-z]+$")
+        match x with
+        |x when reg.IsMatch(x) -> x
+        |x -> printfn " Неверный ввод, только буквы! "; check_Name_Sur_Patr (System.Console.ReadLine())
+
+    let rec check_BirthPlace (x:string) =
+        let reg = new Regex("^([\p{L}\p{N} \.-]+)$")
+        match x with
+        |x when reg.IsMatch(x) -> x
+        |x -> printfn " Неверный ввод(буквы и цифры допустимы)! "; check_BirthPlace (System.Console.ReadLine())
+
 
     member write.inputData =
         printfn " Input serial (6) : "
-        _serial_Passport <- serial_numberList 6
+        _serial_Passport <- serial_NumberPas "serial"
         printfn " Input number (4) : "
-        _number_Passport <- serial_numberList 4
+        _number_Passport <- serial_NumberPas "number"
         printf " Input Surname : "
-        _surnmae_Passport <- System.Console.ReadLine()
+        _surnmae_Passport <- check_Name_Sur_Patr (System.Console.ReadLine())
         printf " Input Name : "
-        _name_Passport <- System.Console.ReadLine()
+        _name_Passport <- check_Name_Sur_Patr (System.Console.ReadLine())
         printf " Input Patronymic : "
-        _patronymic_Passport <- System.Console.ReadLine()
+        _patronymic_Passport <- check_Name_Sur_Patr (System.Console.ReadLine())
         printf " Input Birth Place : "
-        _birthPlace <- System.Console.ReadLine()
-        printf " Input Birth Place (dd.mm.yyyy): "
-        _birthday <- System.Console.ReadLine()
+        _birthPlace <- check_BirthPlace (System.Console.ReadLine())
+        printf " Input BirthDay (dd.mm.yyyy): "
+        _birthday <- checkDateBirth (System.Console.ReadLine())
 
     member print.printDocument =
         printfn " Serial   Number   Surname    Name    Patronymic    birthPlace    birthday\n"
-        let serial = System.String.Concat(_serial_Passport)
-        let number = System.String.Concat(_number_Passport)
-        printfn " %A  %A   %s  %s  %s  %s  %A " serial number _surnmae_Passport _name_Passport _patronymic_Passport _birthPlace _birthday
+        printfn " %s  %s   %s  %s  %s  %s  %A " _serial_Passport _number_Passport _surnmae_Passport _name_Passport _patronymic_Passport _birthPlace _birthday
 
     override this.Equals (x : obj) =
         match x with
@@ -87,10 +80,10 @@ type Document_PassportRF() =
 
     member serial.getSerial
         with get() = _serial_Passport
-        and private set(value:int list) = _serial_Passport <-value 
+        and private set(value : string) = _serial_Passport <- value
     member number.getNumber
         with get() = _number_Passport
-        and private set(value:int list) = _number_Passport <-value
+        and private set(value:string) = _number_Passport <-value
     member surname.getSurname
         with get() = _surnmae_Passport
     member name.getName
@@ -103,8 +96,6 @@ type Document_PassportRF() =
         with get() = _birthday
 
 
-  
-
 [<EntryPoint>]
 let main argv =
     let passport = Document_PassportRF()
@@ -114,14 +105,9 @@ let main argv =
     passport2.inputData
     passport2.printDocument
     printfn "%b" (passport = passport2)
-    (*printfn " %A " passport.getSerial
-    printfn " %A " passport.getNumber
-    printfn " %A " passport.getSurname
-    printfn " %A " passport.getName
-    printfn " %A " passport.getPatronymic
-    printfn " %A " passport.getBirthPlace
-    printfn " %A " passport.getBirthday*)
-    0 // return an integer exit code
+    
+    0 
 (*Task 5 1 April 17 00
   Task 6 2 April 9 43
-  Task 7 4 April 17 00*)
+  Task 7 4 April 16 00
+  Task 8 4 April 17 22*)
