3bebdb7957343810cd57b4ec2c7a8804861cbfd4
StrongerProgrammer7 <96116317+DM-DOOMEnternal@users.noreply.github.com>
2022-04-16 17:37:25 +0300

    Task4
    Задание 14
    Вариант
    1
    Построить
    предикат
    grand_ma_and_son(X,Y),
    который
    проверяет, являются ли X и Y бабушкой и внуком или внуком и бабушкой.

---
+++ SWI-Prolog/LB1/Task4.pl
@@ -0,0 +1,20 @@
+man(petr).
+man(alex).
+man(misha).
+woman(kris).
+woman(katya).
+woman(anna).
+woman(alisa).
+parent(petr,kris).
+parent(anna,kris).
+parent(petr,alex).
+parent(anna,alex).
+grandparent(misha,anna).
+grandparent(katya,anna).
+grandparent(alisa,petr).
+grand_ma_and_son(X,Y):- (grandparent(X,U),woman(X),parent(U,Y),man(Y));
+(parent(U,X),man(X),grandparent(Y,U),woman(Y)).
+grand_mas(X):- grandparent(X,_),woman(X).
+%¬ариант 1 ѕостроить предикат grand_ma_and_son(X,Y),
+% который провер€ет, €вл€ютс€ ли X и Y бабушкой и внуком или внуком и
+% бабушкой
