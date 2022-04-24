24a62ef0034d56aaebc9e13805a6e60be6f2c767
StrongerProgrammer7 <96116317+DM-DOOMEnternal@users.noreply.github.com>
2022-04-24 17:41:54 +0300

    Task 8
    Дан целочисленный массив и натуральный индекс (число, меньшее
    размера массива). Необходимо определить является ли элемент по указан-
    ному индексу локальным минимумом.

---
+++ SWI-Prolog/LB2/Task8.pl
@@ -0,0 +1,21 @@
+%ƒан целочисленный массив и натуральный индекс (число, меньшее
+%размера массива). Ќеобходимо определить €вл€етс€ ли элемент
+%по указанному индексу локальным минимумом.
+%
+
+lengthList(List,L):- lengthList(List,0,L).
+lengthList([],C,L):- L is C,!.
+lengthList([_Head|Tail],Count,L):-
+    Count1 is Count +1,
+    lengthList(Tail,Count1,L).
+
+localMin(List,Ind):-
+    lengthList(List,I), Ind<I,!,localMin(List,Ind,0);false,!.
+localMin([Head|Tail],Ind,CurInd):-
+    CurInd<(Ind-1),!, CurInd1 is CurInd + 1,localMin(Tail,Ind,CurInd1);
+    local(Tail,Head,0).
+local([Head|Tail],MinL,Count):-
+    Count<2,MinL<Head,!,Count1 is Count + 1,local(Tail,Head,Count1);
+    Count=:=2,!,true;
+    MinL>Head,false.
+local([],_MinL,Count):- Count=:=2,true,!.
