18da81bfba0c20765700b32388b77583761d3547
StrongerProgrammer7 <96116317+DM-DOOMEnternal@users.noreply.github.com>
2022-04-24 12:21:12 +0300

    Task1
    Rek Down Add

--- SWI-Prolog/LB2/Task1.pl
+++ SWI-Prolog/LB2/Task1.pl
@@ -14,4 +14,10 @@ sumSimpleDiv(0,_,0):-!.
 sumSimpleDiv(X,Y,S):- Y mod X =:= 0 -> X1 is X-1,
     sumSimpleDiv(X1,Y,S1),(isPrime(X) -> S is S1+X; S is S1);
     Y mod X =\=0 -> X1 is X-1, sumSimpleDiv(X1,Y,S1), S is S1.
-
+%Rekurs down
+sumSimpleDivDown(X,Y):-X>0,!, sumSimpleDivDown(X,X,Y,0),!.
+sumSimpleDivDown(X,Y,S,Z):-
+    X > 0,!,
+    (   Y mod X =:= 0, isPrime(X) -> Z1 is Z+X, X1 is X - 1, sumSimpleDivDown(X1,Y,S,Z1);
+    Z1 is Z, X1 is X-1, sumSimpleDivDown(X1,Y,S,Z1)).
+sumSimpleDivDown(_,_,S,Z):- S is Z.
