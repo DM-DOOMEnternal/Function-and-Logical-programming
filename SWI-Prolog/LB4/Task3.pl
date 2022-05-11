a30d570285dab7d8677507ee61f03cb4c7bb3dd7
StrongerProgrammer7 <96116317+DM-DOOMEnternal@users.noreply.github.com>
2022-05-11 08:23:30 +0300

    LB4 Task3
    Палиндром из строчных символов

--- SWI-Prolog/LB4/Task3.pl
+++ SWI-Prolog/LB4/Task3.pl
@@ -13,5 +13,24 @@ commonCountRus(S,X):-
 
 %Дана строка. Необходимо проверить образуют ли строчные символы латиницы палиндром.
 %
-%
+
+isLowercaseStr([]).
+isLowercaseStr([H|T]):-
+    char_code(H,X),
+    ((X @>=1074,X @=<1103);(X @>=97, X @=<122)),!,isLowercaseStr(T);
+    fail.
+
+palind([],L,L).
+palind([H|T],L,L1):-palind(T,[H|L],L1).
+
+equalList([],[]).
+equalList([H|T],[E|R]):-
+    H=E,!,equalList(T,R);
+    fail.
+
+isPallindrom(S):-
+    string_chars(S,C),
+    isLowercaseStr(C),!,palind(C,[],NL),equalList(C,NL);
+    write('В строке есть другие символы(все дб строчные').
+
 %Найти в тексте даты формата «день.месяц.год».
