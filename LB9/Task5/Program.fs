e87bbaa9cf6024a922052f0bdc2874c698537d59
StrongerProgrammer7 <96116317+DM-DOOMEnternal@users.noreply.github.com>
2022-05-20 21:10:35 +0300

    Исправления

--- LB9/Task5/Program.fs
+++ LB9/Task5/Program.fs
@@ -3,138 +3,44 @@ open System.Drawing
 open System.Windows.Forms
 open System.IO
 
-
-
-let form = new Form()
-// 
-// labelA
-// 
-let labelA = new Label()
-labelA.AutoSize <- true;
-labelA.Location <- new System.Drawing.Point(14, 16);
-labelA.Name <- "labelA";
-labelA.Size <- new System.Drawing.Size(25, 13);
-labelA.TabIndex <- 0;
-labelA.Text <- "a = ";
-form.Controls.Add(labelA)
-// 
-// labelB
-// 
-let labelB = new Label()
-labelB.AutoSize <- true;
-labelB.Location <- new System.Drawing.Point(14, 42);
-labelB.Name <- "labelB";
-labelB.Size <- new System.Drawing.Size(25, 13);
-labelB.TabIndex <- 1;
-labelB.Text <- "b = ";
-form.Controls.Add(labelB)
-// 
-// textBox1
-// 
-let textBox1 = new TextBox()
-textBox1.Location <- new System.Drawing.Point(45, 13);
-textBox1.Name <- "textBox1";
-textBox1.Size <- new System.Drawing.Size(86, 20);
-textBox1.TabIndex <- 2;
-textBox1.Text <- "1";
-form.Controls.Add(textBox1)
-// 
-// textBox2
-// 
-let textBox2 = new TextBox()
-textBox2.Location <- new System.Drawing.Point(45, 39);
-textBox2.Name <- "textBox2";
-textBox2.Size <- new System.Drawing.Size(86, 20);
-textBox2.TabIndex <- 3;
-textBox2.Text <- "2";
-form.Controls.Add(textBox2)
-// 
-// labelAns
-// 
-let labelAns = new Label()
-labelAns.AutoSize <- true;
-labelAns.Location <- new System.Drawing.Point(14, 88);
-labelAns.Name <- "labelAns";
-labelAns.Size <- new System.Drawing.Size(26, 13);
-labelAns.TabIndex <- 5;
-labelAns.Text <- "S = ";
-form.Controls.Add(labelAns)
-// 
-// buttonPl
-// 
-let buttonPl = new Button()
-buttonPl.Location <- new System.Drawing.Point(137, 10);
-buttonPl.Name <- "buttonPl";
-buttonPl.Size <- new System.Drawing.Size(63, 49);
-buttonPl.TabIndex <- 4;
-buttonPl.Text <- "Найти площадь";
-buttonPl.UseVisualStyleBackColor <- true;
-form.Controls.Add(buttonPl)
-let Ans _ = 
-    let strAns = 
-        try
-            "S = " + Convert.ToString(int(textBox1.Text) * (int(textBox2.Text)))
-        with
-            | :? System.TypeInitializationException -> 
-                printfn "Одно из полей оказалось пустым!"
-                "None"
-    labelAns.Text <- strAns
-buttonPl.Click.Add(Ans)
-// 
-// Form1
-// 
-form.AutoScaleDimensions <- new System.Drawing.SizeF(6F, 13F);
-form.AutoScaleMode <- System.Windows.Forms.AutoScaleMode.Font;
-form.ClientSize <- new System.Drawing.Size(217, 121);
-form.Controls.Add(labelAns);
-form.Controls.Add(buttonPl);
-form.Controls.Add(textBox2);
-form.Controls.Add(textBox1);
-form.Controls.Add(labelB);
-form.Controls.Add(labelA);
-form.Name <- "Form1";
-form.Text <- "Form1";
-form.ResumeLayout(false);
-form.PerformLayout();
-
-
-
-// Запускаем форму
-do Application.Run(form)
-
+(*1. Разработать программу, состоящую из трех форм. 
+На главной форме находится меню (MainMenu), через которое осуществляется переход на другие формы.*)
 [<EntryPoint>]
 let main argv =
-    0 // return an integer exit code
+    let form = new Form(Width = 815, Height = 452, Text = "Меню", Menu = new MainMenu())
+    let button1 = new Button(Text = "начало", Top = 100, Left = 200, Width = 400, Height = 140,BackColor = System.Drawing.Color.Red,
+    Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204))))
 
-(*Разработать программу, состоящую из трех форм. На
-главной форме находится меню (MainMenu), через которое
-осуществляется переход на другие формы.
-2. Разработать программу решающую квадратное уравнение,
-состоящую из одной формы. Ответ выводится в RichTextBox.
-3. Разработать программу, реализующую простые вычис-
-ление над двумя действительными числами (сложения, вычита-
-ния, деления, умножения). Учесть все возможные ошибки.
-4. Разработать программу, состоящую из главной формы,
-рисунка и одной кнопки. По нажатию на кнопку меняется ри-
-сунок.
-5. Разработать программу, которая по выбору месяца выда-
-вала сообщение со временем года. Главная форма, а на ней вы-
-падающий список с месяцами, сообщение появляется при
-нажатии на кнопку.
-Разработать приложение, состоящее из одной формы,
-на которой размещены кнопка и ползунок. При изменении по-
-ложения ползунка меняется ширина кнопки.
-7. Разработать программу, состоящую из главной формы
-на которой размещены два флажка и одна кнопка. По нажатию
-на кнопку появляется сообщение «установлен первый флажок»,
-«установлен второй флажок», «установлены оба флажка».
-8. Разработать программу, состоящую из одной формы, на
-которой размещены: поле для ввода, кнопка и список элемен-
-тов. После того как ввели текст в поле ввода нажатием на кноп-
-ку он добавляется в список.
-9. Разработать программу, на главной форме разместить
-поле для ввода и индикатор хода загрузки. По мере ввода текста
-в поле ввода заполняется индикатор.
-10. Разработать приложение, вычисляющее площадь пря-
-моугольника.
-*)
+    //Ф 1 
+    let form1 = new Form(Text = "Жизнь проста", Width = 450, Height = 300)
+    let label1 = new Label(Text = "Ваш ненавидимый ЯП?", AutoSize = true, Location = new System.Drawing.Point(60, 50),
+    Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204))))
+    form1.Controls.Add(label1)
+    let opForm1 EventsArgs = do form1.ShowDialog()
+    button1.Click.Add opForm1
+    form.Controls.Add(button1)
+    let button2 = new Button(Text = "F#1", Top=125, Left = 50, Width = 100, Height = 50)
+    let button3 = new Button(Text = "Или все такие F#!.", Top = 125, Left = 250, Width = 100, Height = 50)
+    form1.Controls.Add(button2)
+    form1.Controls.Add(button3)
+
+    //Ф 2 
+    let form2 = new Form(Text= "Минус", Width = 300, Height = 160)
+    let opForm2 EventsArgs = do form2.ShowDialog()
+    button2.Click.Add(opForm2)
+    let label2 = new Label(Text = "Такой минус",AutoSize = true, Location = new System.Drawing.Point(60, 50),
+    Font = new System.Drawing.Font("Microsoft Sans Serif", 9.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204))))
+    form2.Controls.Add(label2)
+    
+    //Ф 3
+    let  = new Form(Text= "Стоп", Width = 300, Height = 160)
+    let opForm3 EventsArgs = do form3.ShowDialog()
+    button3.Click.Add(opForm3)
+    let label3 = new Label(Text = "Ты кто?",AutoSize = true, Location = new System.Drawing.Point(60, 50),
+    Font = new System.Drawing.Font("Microsoft Sans Serif", 9.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204))))
+    form3.Controls.Add(label3)
+
+    do Application.Run(form)
+    0 
+	
+	
---
+++ LB9/Task6/Program.fs
@@ -0,0 +1,152 @@
+﻿open System
+open System.Windows
+open System.Windows.Controls
+open System.Windows.Markup
+
+//Разработать программу, состоящую из трех форм. 
+//На главной форме находится меню, через которое осуществляется переход на другие формы.
+
+//Главная форма с меню
+let mwXaml = "
+<Window
+xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'
+xmlns:x='http://schemas.microsoft.com/winfx/2006/xaml'
+Title='MainForm' Height='285' Width='320'>
+<Grid>
+<Grid.ColumnDefinitions>
+<ColumnDefinition Width='156*' />
+<ColumnDefinition Width='163' />
+</Grid.ColumnDefinitions>
+<Button Content='Сегодня ПНД' Height='75' HorizontalAlignment='Left' Margin='
+12,10,0,0' Name='button9' VerticalAlignment='Top' Width='286' 
+Grid.ColumnSpan='2' />
+<Button Content='Что то на эврите' Height='75' HorizontalAlignment='Left' Margin='
+12,86,0,0' Name='button10' VerticalAlignment='Top' Width='286'
+Grid.ColumnSpan='2' />
+<Button Content='Выход!' Height='25' HorizontalAlignment='Left' Margin='
+12,160,0,0' Name='button11' VerticalAlignment='Top' Width='286'
+Grid.ColumnSpan='2' Background = 'LightGreen'/>
+<Button Content='Формы Ф шарп' Height='55' HorizontalAlignment='
+Left' Margin='12,160,0,0' Name='button12' VerticalAlignment='Top'
+Width='286' Grid.ColumnSpan='2' />
+</Grid>
+</Window>
+"
+// загрузка разметки XAML
+let getWindow(mwXaml) = let xamlObj=XamlReader.Parse(mwXaml)
+                        xamlObj :?> Window
+let win = getWindow(mwXaml)
+//получение экземпляров элементов управления
+//главная форма:
+let button9 = win.FindName("button9") :?> Button  // ф 1 
+let button10 = win.FindName("button10") :?> Button // ф 2
+let button11 = win.FindName("button11") :?> Button // ф 3
+let button12 = win.FindName("button12") :?> Button // ф 4-выход
+//обработка событий
+button12.Click.AddHandler(fun _ _ -> win.Close())//выход
+
+//Форма 3
+let form3= "
+<Window
+xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'
+    xmlns:x='http://schemas.microsoft.com/winfx/2006/xaml'
+    Title='New Window' Height='221' Width='351'>
+<Grid>
+    <Grid.ColumnDefinitions>
+        <ColumnDefinition Width='156*' />
+        <ColumnDefinition Width='163' />
+    </Grid.ColumnDefinitions>
+ <Button Content='СДАЕМ ЛАБЫ!' Height='75' HorizontalAlignment='Left' Margin='
+12,10,0,0' Name='button1' VerticalAlignment='Top' Width='286' 
+Grid.ColumnSpan='2' />
+    <Button Content='Выход' Height='23' HorizontalAlignment='Left' Margin='
+12,153,0,0' Name='button2' VerticalAlignment='Top' Width='305'
+Grid.ColumnSpan='2' />
+    <Label Content=' ' Grid.Column='1' Height='28' HorizontalAlignment='
+Left' Margin='65,50,0,0' Name='label1' VerticalAlignment='Top' />
+    </Grid>
+</Window>
+"
+let getWindow3(form3) = 
+    let xamlObj=XamlReader.Parse(form3) 
+    xamlObj :?> Window
+let wind3 = getWindow3(form3)
+let nb13 = wind3.FindName("button1") :?> Button
+let nb23 = wind3.FindName("button2") :?> Button
+button12.Click.AddHandler(fun _ _ -> wind3.Show()|>ignore)
+nb23.Click.AddHandler(fun _ _ -> wind3.Hide())
+
+//Форма 2
+let form2= "
+<Window
+xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'
+    xmlns:x='http://schemas.microsoft.com/winfx/2006/xaml'
+    Title='New Window' Height='221' Width='351'>
+<Grid>
+    <Grid.ColumnDefinitions>
+        <ColumnDefinition Width='156*' />
+        <ColumnDefinition Width='163' />
+    </Grid.ColumnDefinitions>
+ <Button Content=' Что то новое' Height='75' HorizontalAlignment='Left' Margin='
+12,10,0,0' Name='button1' VerticalAlignment='Top' Width='286' 
+Grid.ColumnSpan='2' />
+    <Button Content='Выход' Height='23' HorizontalAlignment='Left' Margin='
+12,153,0,0' Name='button2' VerticalAlignment='Top' Width='305'
+Grid.ColumnSpan='2' />
+    <Label Content=' ' Grid.Column='1' Height='28' HorizontalAlignment='
+Left' Margin='65,50,0,0' Name='label1' VerticalAlignment='Top' />
+    </Grid>
+</Window>
+"
+let getWindow2(form2) = 
+    let xamlObj=XamlReader.Parse(form2) 
+    xamlObj :?> Window
+let wind2 = getWindow2(form2)
+let nb12 = wind2.FindName("button1") :?> Button
+let nb22 = wind2.FindName("button2") :?> Button
+button10.Click.AddHandler(fun _ _ -> wind2.Show()|>ignore)
+nb12.Click.AddHandler(fun _ _ -> wind3.Show()|>ignore)
+nb22.Click.AddHandler(fun _ _ -> wind2.Hide())
+
+//Форма 1
+let form1= "
+<Window
+xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'
+    xmlns:x='http://schemas.microsoft.com/winfx/2006/xaml'
+    Title='New Window' Height='221' Width='351'>
+<Grid>
+    <Grid.ColumnDefinitions>
+        <ColumnDefinition Width='156*' />
+        <ColumnDefinition Width='163' />
+    </Grid.ColumnDefinitions>
+ <Button Content=' Новая формочка!' Height='75' HorizontalAlignment='Left' Margin='
+12,10,0,0' Name='button1' VerticalAlignment='Top' Width='286' 
+Grid.ColumnSpan='2' />
+    <Button Content='Выход' Height='23' HorizontalAlignment='Left' Margin='
+12,153,0,0' Name='button2' VerticalAlignment='Top' Width='305'
+Grid.ColumnSpan='2' />
+    <Label Content=' ' Grid.Column='1' Height='28' HorizontalAlignment='
+Left' Margin='65,50,0,0' Name='label1' VerticalAlignment='Top' />
+    </Grid>
+</Window>
+"
+let getWindow1(form1) = 
+    let xamlObj=XamlReader.Parse(form1) 
+    xamlObj :?> Window
+let wind = getWindow1(form1)
+let nb1 = wind.FindName("button1") :?> Button
+let nb2 = wind.FindName("button2") :?> Button
+button9.Click.AddHandler(fun _ _ -> wind.Show()|>ignore)
+nb1.Click.AddHandler(fun _ _ -> wind2.Show()|>ignore)
+nb2.Click.AddHandler(fun _ _ -> wind.Hide())
+
+
+[<STAThread>] 
+[<EntryPoint>]
+let main argv =
+    ignore <| (new Application()).Run win
+    0
+	
+	(*Разработать программу, состоящую из трех форм. На
+главной форме находится меню через которое осуществляется
+переход на другие формы.*)
